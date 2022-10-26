134b8d2f93fefc6a6261cc359b19bbacf7aa595c
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-10-26 22:54:50 +0300

    react router tehtävä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AboutComponent from "./components/about";
+import HomeComponent from "./components/home";
+import ContactComponent from "./components/contact";
+import NotFoundComponent from "./components/NotFoundComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor: "black", color: "white" }}>
+        <h1>Welcome to React Router</h1>
+      </div>
+      <BrowserRouter>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>{" "}
+        <Link to="/contact">Contact</Link>{" "}
+        <Routes>
+          <Route path="/" element={<HomeComponent />} />
+          <Route path="/about" element={<AboutComponent />} />
+          <Route path="/contact" element={<ContactComponent />} />
+          <Route path="*" element={<NotFoundComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
